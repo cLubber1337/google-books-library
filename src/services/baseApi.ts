6b8cd0d1436a120c509604d6be7ce9f71331f3bf
Andrey Soroko <supersoroko@gmail.com>
2023-09-07 20:14:52 +0300

    Setup project

---
+++ src/services/baseApi.ts
@@ -0,0 +1,13 @@
+import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query";
+
+
+const apiKey = process.env.BOOKS_API_KEY;
+
+export const baseApi = createApi({
+    reducerPath: 'baseApi',
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'https://www.googleapis.com/books/v1',
+        credentials: 'include',
+    }),
+    endpoints: () => ({}),
+})
